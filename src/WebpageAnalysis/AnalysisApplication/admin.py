b8fb0fbc9cd86b865e0c02d90fb6d7bfc91e1f4c
Raivo Koot <raivokoot@gmail.com>
2019-05-06 14:40:21 +0100

    added models

--- src/WebpageAnalysis/AnalysisApplication/admin.py
+++ src/WebpageAnalysis/AnalysisApplication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import WebPage, Heading
 
 # Register your models here.
+
+admin.site.register(WebPage)
+admin.site.register(Heading)
--- src/WebpageAnalysis/AnalysisApplication/models.py
+++ src/WebpageAnalysis/AnalysisApplication/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 # Create your models here.
+class WebPage(models.Model):
+    html_version = models.CharField(max_length=20)
+    page_title = models.CharField(max_length=100)
+    internal_links = models.IntegerField()
+    external_links = models.IntegerField()
+    inaccessible_links = models.IntegerField()
+    has_loginform = models.BooleanField()
+
+    # the date on which this database row needs to be deleted:
+    # 24 hours after creation
+    delete_on = models.DateTimeField(default=datetime.now()+timedelta(days=1))
+
+class Heading(models.Model):
+    HEADING_TYPE_CHOICES = (
+        ('H1', 'Heading Type 1'),
+        ('H2', 'Heading Type 2'),
+        ('H3', 'Heading Type 3'),
+        ('H4', 'Heading Type 4'),
+        ('H5', 'Heading Type 5'),
+        ('H6', 'Heading Type 6')
+    )
+
+    type = models.CharField(
+        max_length=2,
+        choices=HEADING_TYPE_CHOICES
+    )
+    count = models.IntegerField()
+    webpage = models.ForeignKey(WebPage, on_delete=models.CASCADE)

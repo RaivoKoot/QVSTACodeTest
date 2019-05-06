c953c4b429b96ff14cd4971fa91fe5135e7a17c0
Raivo Koot <raivokoot@gmail.com>
2019-05-06 17:56:27 +0100

    Added url parameter to GET request. Will return newest row or return a message and 400 error code if url query parameter is missing

--- src/WebpageAnalysis/AnalysisApplication/views.py
+++ src/WebpageAnalysis/AnalysisApplication/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from AnalysisApplication.models import WebPage, Heading
 from AnalysisApplication.serializers import WebPageSerializer, HeadingSerializer
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
-class WebPageList(generics.ListCreateAPIView):
-    queryset = WebPage.objects.all()
+class WebPageList(generics.ListAPIView):
     serializer_class = WebPageSerializer
+
+    def get_queryset(self):
+        queryset = WebPage.objects.all()
+        url = self.request.query_params.get('url', None)
+
+        if url is not None:
+            # reverse ordering so that the newest is the first
+            # and then only take the first object
+            queryset = queryset.filter(url=url).order_by('-pk')[:1]
+        else:
+            raise ValidationError('The query parameter "url" was not passed.')
+
+        return queryset
